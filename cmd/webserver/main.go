3148ca60f38d0be4b4e02ac889e00bcad84722e2
Charlie_Chiu <marmot00@gmail.com>
2020-04-10 14:18:49 +0800

    make server works

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -8,8 +8,43 @@ import (
 	"github.com/charlie-chiu/gode"
 )
 
+type StubPhpGame struct {
+	ReadyMessage            string
+	LoginMessage            string
+	LoadInfoMessage         string
+	TakeMachineMessage      string
+	GetMachineDetailMessage string
+}
+
+func (s StubPhpGame) OnReady() string {
+	return s.ReadyMessage
+}
+
+func (s StubPhpGame) OnTakeMachine() string {
+	return s.TakeMachineMessage
+}
+
+func (s StubPhpGame) OnLoadInfo() string {
+	return s.LoadInfoMessage
+}
+
+func (s StubPhpGame) OnGetMachineDetail() string {
+	return s.GetMachineDetailMessage
+}
+
+func (s StubPhpGame) OnLogin() string {
+	return s.LoginMessage
+}
+
 func main() {
-	server := gode.NewWSServer()
+	stubGame := StubPhpGame{
+		ReadyMessage:            "OnReady",
+		LoginMessage:            "OnLogin",
+		LoadInfoMessage:         "OnLoadInfo",
+		TakeMachineMessage:      "OnTakeMachine",
+		GetMachineDetailMessage: "OnGetMachineDetail",
+	}
+	server := gode.NewWSServer(stubGame)
 
 	fmt.Println("start listen...")
 	err := http.ListenAndServe(":80", server)
