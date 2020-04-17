7adc0bcd43cf4c0edacf84cbe84713c15d91a5cf
Charlie_Chiu <marmot00@gmail.com>
2020-04-17 17:53:10 +0800

    WIP

--- flash2db_php_game.go
+++ flash2db_php_game.go
@@ -1,26 +1,43 @@
 package gode
 
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
 const PathPrefix = "/amfphp/json.php"
 const Path5145 = "/casino.slot.line243.BuBuGaoSheng."
 
 type Flash2dbPhpGame struct {
-	GameType GameType
+	url string
 }
 
-func NewFlash2dbPhpGame(gameType GameType) *Flash2dbPhpGame {
+func NewFlash2dbPhpGame(host string, gameType GameType) *Flash2dbPhpGame {
 	path := map[GameType]string{
 		5145: Path5145,
 	}
 
-	_ = PathPrefix + path[5145]
+	url := host + PathPrefix + path[5145]
 
 	return &Flash2dbPhpGame{
-		GameType: gameType,
+		url: url,
 	}
 }
 
-func (Flash2dbPhpGame) OnTakeMachine() []byte {
-	panic("implement me")
+func (g *Flash2dbPhpGame) OnTakeMachine() []byte {
+	response, err := http.Get(g.url)
+	if err != nil {
+		log.Fatal("http Get Error", err)
+	}
+	defer response.Body.Close()
+
+	bytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Print("ioutil ReadAll error : ", err)
+	}
+
+	return bytes
 }
 
 func (Flash2dbPhpGame) OnLoadInfo() []byte {
--- flash2db_php_game_test.go
+++ flash2db_php_game_test.go
@@ -1,9 +1,31 @@
 package gode
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
 func TestFlash2dbPhpGame(t *testing.T) {
-	_ = NewFlash2dbPhpGame(5145)
+	t.Run("OnTakeMachine return []byte", func(t *testing.T) {
+		const path = "/amfphp/json.php/casino.slot.line243.BuBuGaoSheng.machineOccupyAuto/362907402"
+		handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprint(w, `help`)
+		})
+		srv := httptest.NewServer(handler)
+		httptest.
+		defer srv.Close()
+
+
+		host := srv.URL
+		host = host + "/hello"
+		fmt.Println(host)
+		g := NewFlash2dbPhpGame(host, 5146)
+
+		want := []byte(`help`)
+		got := g.OnTakeMachine()
+
+		assertByteEqual(t, got, want)
+	})
 }
