392a6facac857451dd35529943da67c2d2498979
charlie <marmot00@gmail.com>
2020-04-19 11:28:53 +0800

    make it work

--- flash2db_php_game_test.go
+++ flash2db_php_game_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestFlash2dbPhpGame(t *testing.T) {
-	t.Run("OnTakeMachine return []byte", func(t *testing.T) {
+	t.Run("OnTakeMachine get correct url and return result", func(t *testing.T) {
 		const wantedURL = "/amfphp/json.php/casino.slot.line243.BuBuGaoSheng.machineOccupyAuto/362907402"
 		handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			assertURLEqual(t, r, wantedURL)
@@ -17,7 +17,7 @@ func TestFlash2dbPhpGame(t *testing.T) {
 		srv := httptest.NewServer(handler)
 		defer srv.Close()
 
-		g := NewFlash2dbPhpGame(srv.URL, 5146)
+		g := NewFlash2dbPhpGame(srv.URL, 5145)
 
 		want := []byte(`help`)
 		got := g.OnTakeMachine()
